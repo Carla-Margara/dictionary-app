d49648d95c312a0e7d047cbaf4b407e3560f290d
Carla-Margara <90868430+Carla-Margara@users.noreply.github.com>
2022-01-31 11:57:54 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logoWhite from "./logoWhite.png";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logoWhite} className="App-logo img-fluid" alt="logo" />
+      </header>
+    </div>
+  );
+}
