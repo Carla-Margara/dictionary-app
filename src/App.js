1a749c7ef596e9fb47855aaa4f05a604b0c1dad1
Carla-Margara <90868430+Carla-Margara@users.noreply.github.com>
2022-01-31 13:15:49 +0000

    Created Dictionary.js and Dictionary.css
    Added a form and search function and updated App.js and App.css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import logoWhite from "./logoWhite.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logoWhite} className="App-logo img-fluid" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logoWhite} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+
+        <footer className="text-center">
+          Coded with <span className="heart">❤</span> by Carla Margara
+        </footer>
+      </div>
     </div>
   );
 }
