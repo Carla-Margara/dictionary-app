f81b7f280fb2a0a7c779c4e972af19946799e582
Carla-Margara <90868430+Carla-Margara@users.noreply.github.com>
2022-02-11 16:43:47 +0000

    Updated Dictionary.css, Meaning.css and Meaning.js to change display and add the Synonyms component

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Meaning.css";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   return (
@@ -8,9 +9,14 @@ export default function Meaning(props) {
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
-            <p>{definition.definition}</p>
             <p>
+              <strong>Definition: </strong>
+              {definition.definition}
+              <br />
+              <strong>Example: </strong>
               <em>{definition.example}</em>
+              <br />
+              <Synonyms synonyms={definition.synonyms} />
             </p>
           </div>
         );
